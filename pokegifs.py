856fc85be1579fbe09cf30307ed80a1e50d412de
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-20 14:46:49 -0400

    tested some stuff out with the pokeapi and giphy, pulling my key from an environment variable, displaying some information about a pokemon and pulling a single gif and saving it to a variable

---
+++ pokegifs.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import os
+
+giphy_key = os.environ.get("GIPHY_KEY")
+pokemon_name = 'pikachu'
+
+poke_response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+poke_body = json.loads(poke_response.content)
+print(f"The name is {poke_body['name']}")
+for type in poke_body['types']:
+    print(f"This pokemon has a type of {type['type']['name']}")
+
+
+giphy_response = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={giphy_key}&q={pokemon_name}&limit=1")
+giphy_body = json.loads(giphy_response.content)
+url = giphy_body['data'][0]['url']
+print(f"The location of the gif is: {url}")

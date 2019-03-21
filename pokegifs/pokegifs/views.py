042ba4302cdad00b3538f8ad29ff0c3651fd6170
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-21 13:09:19 -0400

    added handling for an invalid key being used

--- pokegifs/pokegifs/views.py
+++ pokegifs/pokegifs/views.py
@@ -1,4 +1,3 @@
-# import ipdb
 import random
 import requests
 import json
@@ -17,14 +16,13 @@ def pokemon(request, id):
     for type in body['types']:
         types.append(type['type']['name'])
     giphy_response = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={giphy_key}&q={name}")
-    giphy_body = json.loads(giphy_response.content)
-    rand_num = random.randint(0, len(giphy_body['data'])-1)
-    print(rand_num)
-    print(len(giphy_body['data']))
-    gif_url = giphy_body['data'][rand_num]['url']
-    # ipdb.set_trace()
-    # print(f"The location of the gif is: {gif_url}")
-    return JsonResponse({'id': id, 'name': name, 'types': types, 'gif': gif_url})
+    if giphy_response.status_code == 403:
+        return JsonResponse({'error': 'Encountered a 403 error retrieving gif'})
+    else:
+        giphy_body = json.loads(giphy_response.content)
+        rand_num = random.randint(0, len(giphy_body['data'])-1)
+        gif_url = giphy_body['data'][rand_num]['url']
+        return JsonResponse({'id': id, 'name': name, 'types': types, 'gif': gif_url})
 
 
 def team(request):
@@ -39,16 +37,17 @@ def team(request):
         for type in body['types']:
             types.append(type['type']['name'])
         giphy_response = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={giphy_key}&q={name}")
-        giphy_body = json.loads(giphy_response.content)
-        if len(giphy_body['data']) == 0:
-            gif_url = ''
+        if giphy_response.status_code == 403:
+            return JsonResponse({'error': 'Encountered a 403 error retrieving gif'})
         else:
-            rand_num = random.randint(0, len(giphy_body['data'])-1)
-            gif_url = giphy_body['data'][rand_num]['url']
-        pokemon = {'id': id, 'name': name, 'types': types, 'gif': gif_url}
-        print(pokemon)
-        idx = f"pk{num}"
-        list_of_monsters[idx] = pokemon
-    print(list_of_monsters)
-
+            giphy_body = json.loads(giphy_response.content)
+            if len(giphy_body['data']) == 0:
+                gif_url = ''
+            else:
+                rand_num = random.randint(0, len(giphy_body['data'])-1)
+                gif_url = giphy_body['data'][rand_num]['url']
+            pokemon = {'id': id, 'name': name, 'types': types, 'gif': gif_url}
+            print(pokemon)
+            idx = f"pk{num}"
+            list_of_monsters[idx] = pokemon
     return JsonResponse(list_of_monsters)
